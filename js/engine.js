b6a63484fdf8000f4ee17ea3b8a51649c23f9d7e
drakhshaniaram <drakhshani.aram@gmail.com>
2014-04-14 09:44:43 +0430

    Add main.html
    انتقال اسپلش به صفحه مین

--- js/engine.js
+++ js/engine.js
@@ -178,26 +178,7 @@ function send2Server(art_id, cat_id, user_name, user_family, textval) {
       }
    });
 }
-//__________________________________________ Show the Splash Screen___________________________________________
-//<![CDATA[ 
-$(window).load(function () {
-   $(function () {
-      setTimeout(hideSplash, 5000);
-   });
 
-   function hideSplash() {
-      $.mobile.changePage("#mainPage", "flip");
-      $(".splash").remove();
-   }
-
-   $.fn.center = function () {
-      this.css("position", "absolute");
-      this.css("top", Math.max(0, (($(window).height() - this.outerHeight()) / 2) + $(window).scrollTop()) + "px");
-      this.css("left", Math.max(0, (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft()) + "px");
-      return this;
-   };
-   $("#splashImg").center();
-}); //]]> 
 //__________________________________________ Controller function  __________________________________________________
 $(document).ready(function () {
    function getContentFromServer() {
